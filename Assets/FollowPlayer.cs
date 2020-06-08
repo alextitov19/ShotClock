9b173b7b5553dc45c5007794b1652a80b6ed35a0
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-06-08 14:01:00 -0700

    added stuff

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public Vector3 rotation;
+
+
+    private void Start()
+    {
+        transform.eulerAngles = transform.eulerAngles + rotation;
+    }
+
+    void Update()
+    {
+        transform.rotation = player.rotation;
+        transform.position = player.position + offset;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody playerBody;
+    public Vector3 movementVector;
+    public float rotationSpeed;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerBody.transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed, 0);
+
+        if (Input.GetKey("d"))
+        {
+            playerBody.transform.Translate(movementVector.x * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            playerBody.transform.Translate(-movementVector.x * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            playerBody.transform.Translate(0, 0, movementVector.z * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            playerBody.transform.Translate(0, 0, -movementVector.z * Time.deltaTime);
+        }
+
+
+
+    }
+}
