742f09454a93820ae3cc9812567c47416a58ef85
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-06-09 12:10:38 -0700

    bot collisions register

--- Assets/BotScript.cs
+++ Assets/BotScript.cs
@@ -3,12 +3,27 @@
 public class BotScript : MonoBehaviour
 {
     public Transform player;
-    public Rigidbody bullet;
+
     public int movementSpeed;
     void Update()
     {
         transform.LookAt(player);
         transform.Translate(0, 0, movementSpeed * Time.deltaTime);
+
         
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Debug.Log("Bot collided with bullet");
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Bot collided with player");
+        }
     }
 }
