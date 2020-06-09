1c45c238cc4cf8acf649133f242c891ea33337f7
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-06-09 11:11:39 -0700

    bot added

---
+++ Assets/BotMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotMovement : MonoBehaviour
+{
+    public Transform player;
+    public int movementSpeed;
+    void Update()
+    {
+        transform.LookAt(player);
+        transform.Translate(0, 0, movementSpeed * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/Gameplay.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    public Rigidbody playerBody;
+    public Rigidbody bulletBody;
+    public Vector3 movementVector;
+    public float rotationSpeed;
+    public float bulletSpeed;
+        
+    void Update()
+    {
+        playerBody.transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed, 0);
+
+        if (Input.GetKey("d"))
+        {
+            playerBody.transform.Translate(movementVector.x * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            playerBody.transform.Translate(-movementVector.x * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            playerBody.transform.Translate(0, 0, movementVector.z * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            playerBody.transform.Translate(0, 0, -movementVector.z * Time.deltaTime);
+        }
+
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed primary button.");
+            bulletBody.transform.position = new Vector3(playerBody.transform.position.x, 1, playerBody.transform.position.z);
+            bulletBody.transform.eulerAngles = new Vector3(0, playerBody.transform.eulerAngles.y, 0);
+            InvokeRepeating("moveBullet", 0.0f, 0.01f);
+        }
+
+
+    }
+
+    void moveBullet()
+    {
+        bulletBody.transform.Translate(0, 0, bulletSpeed * Time.deltaTime);
+    }
+}
